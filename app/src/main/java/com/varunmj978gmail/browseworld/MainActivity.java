34dadfe58c3b3d479ecd695fa8cbc7694b59155d
varunmj <varunmj978@gmail.com>
2016-02-26 00:58:28 +0530

    Initial commit

---
+++ app/src/main/java/com/varunmj978gmail/browseworld/MainActivity.java
@@ -0,0 +1,90 @@
+package com.varunmj978gmail.browseworld;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+        EditText et;
+        WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et=(EditText)findViewById(R.id.editText);
+        wv=(WebView)findViewById(R.id.webView);
+        wv.loadUrl("https://www.google.com");
+        WebSettings ws= wv.getSettings();
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new ourWebClient());
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode==KeyEvent.KEYCODE_BACK && wv.canGoBack()){
+            wv.goBack();
+            return true;
+        }
+        else{
+            return super.onKeyUp(keyCode, event);
+        }
+    }
+
+    class ourWebClient extends WebViewClient{
+         @Override
+         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+             wv.loadUrl(url);
+             return true;
+             //return super.shouldOverrideUrlLoading(view, url);
+         }
+     }
+    public void loadMySite(View v) {
+        String url = et.getText().toString();
+        if (!url.equals("")) {
+            wv.loadUrl(url);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
